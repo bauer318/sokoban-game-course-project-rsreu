878360f6bb820abd63affa2711848dde80469ef1
Jacques kibamba <jacqueskibamba@gmail.com>
2022-12-14 18:58:02 +0300

    set WindowsStyle to none and WindowsStartupLocation to CenterScreen, so deleted hidden's button code

--- ViewWPF/MainWindow.cs
+++ ViewWPF/MainWindow.cs
@@ -11,35 +11,14 @@ namespace ViewWPF
 {
     public partial class MainWindow:Window
     {
-        [DllImport("user32.dll")]
-        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
-
-        [DllImport("user32.dll")]
-        static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
-
-        const uint MF_BYCOMMAND = 0x00000000;
-        const uint MF_GRAYED = 0x00000001;
-
-        const uint SC_CLOSE = 0xF060;
-
         public MainWindow()
         {
             ShowActivated = true;
             Width = 720;
             Height = 450;
             ResizeMode = ResizeMode.NoResize;
-        }
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            base.OnSourceInitialized(e);
-
-            // Disable close button
-            IntPtr hwnd = new WindowInteropHelper(this).Handle;
-            IntPtr hMenu = GetSystemMenu(hwnd, false);
-            if (hMenu != IntPtr.Zero)
-            {
-                EnableMenuItem(hMenu, SC_CLOSE, MF_BYCOMMAND | MF_GRAYED);
-            }
+            WindowStyle = WindowStyle.None;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
     }
 }
