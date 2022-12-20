12a68842df4a21427cdb3ad7088795e51c9cea40
Jacques kibamba <jacqueskibamba@gmail.com>
2022-12-21 02:13:43 +0300

    Added play game's model base. ViewNewGameBase and ControllerPlayGame

---
+++ Model/PlayGame/Cell/FloorCell.cs
@@ -0,0 +1,45 @@
+﻿using Model.PlayGame.Levels;
+using Model.PlayGame.Locations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.PlayGame.Cell
+{
+    public class FloorCell : Cell
+    {
+		const string cellName = "Floor";
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FloorCell"/> class.
+		/// </summary>
+		/// <param name="location">The location of the cell.</param>
+		/// <param name="level">The level on which the cell is located.</param>
+		public FloorCell(Location location, Level level)
+			: base(cellName, location, level)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FloorCell"/> class.
+		/// </summary>
+		/// <param name="location">The location of the cell.</param>
+		/// <param name="level">The level on which the cell is located.</param>
+		/// <param name="contents">The contents of this cell.</param>
+		public FloorCell(Location location, Level level, CellContents contents)
+			: base(cellName, location, level, contents)
+		{
+		}
+		public override void RemoveContents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool TrySetContents(CellContents contents)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Model/PlayGame/Locations/DirectionMethods.cs
@@ -0,0 +1,37 @@
+﻿using Model.PlayGame.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.PlayGame.Locations
+{
+	/// <summary>
+	/// Adds some static auxiliary methods
+	/// </summary>
+	public static class DirectionMethods
+	{
+		/// <summary>
+		/// Gets the opposite direction for the direction.
+		/// </summary>
+		/// <param name="direction">The direction.</param>
+		/// <returns></returns>
+		public static Direction GetOppositeDirection(this Direction direction)
+		{
+			switch (direction)
+			{
+				case Direction.Up:
+					return Direction.Down;
+				case Direction.Down:
+					return Direction.Up;
+				case Direction.Left:
+					return Direction.Right;
+				case Direction.Right:
+					return Direction.Left;
+				default:
+					throw new SokobanException("Invalid direction: " + direction.ToString("G"));
+			}
+		}
+	}
+}
