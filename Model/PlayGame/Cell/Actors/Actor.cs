58d1553462b1b76b5b8282feb5f7a5e74eab5b88
Jacques kibamba <jacqueskibamba@gmail.com>
2022-12-21 02:13:43 +0300

    Added play game's model base. ViewNewGameBase and ControllerPlayGame

---
+++ Model/PlayGame/Cell/Actors/Actor.cs
@@ -0,0 +1,72 @@
+﻿using Model.PlayGame.Levels;
+using Model.PlayGame.Locations;
+using Model.PlayGame.Moves;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.PlayGame.Cell.Actors
+{
+    public partial class Actor: CellContents
+    {
+		private readonly Stack<MoveBase> _moves = new Stack<MoveBase>();
+		private int _moveCount;
+		/* lock object for the DoMove methods. */
+		private readonly object _moveLock = new object();
+
+		/// <summary>
+		/// Gets the move count.
+		/// </summary>
+		/// <value>The number of _moves (or steps)
+		/// that the actor has completed.</value>
+		public int MoveCount
+		{
+			get
+			{
+				return _moveCount;
+			}
+			private set
+			{
+				_moveCount = value;
+				if (_moveCount < 0)
+				{   /* Just in case. */
+					_moveCount = 0;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Actor"/> class.
+		/// </summary>
+		/// <param name="location">The location.</param>
+		/// <param name="level">The level.</param>
+		public Actor(Location location, Level level)
+			: base("Actor", location, level)
+		{
+		}
+
+		/// <summary>
+		/// Undoes the last move.
+		/// The move may be a single move, or it may be a series
+		/// of _moves that were taken as part of a <see cref="Jump"/>.
+		/// </summary>
+		/// <returns></returns>
+		public bool UndoMove()
+		{
+			if (_moves.Count < 1)
+			{
+				return false;
+			}
+			MoveBase moveBase = _moves.Pop();
+			Move move = moveBase as Move;
+			if (move != null)
+			{
+				return DoMove(move);
+			}
+			
+			return false;
+		}
+	}
+}
---
+++ Model/PlayGame/Commands/MoveCommand.cs
@@ -0,0 +1,73 @@
+﻿using Model.PlayGame.Levels;
+using Model.PlayGame.Locations;
+using Model.PlayGame.Moves;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.PlayGame.Commands
+{
+    /// <summary>
+	/// Performs a move with the <see cref="Level"/>'s <see cref="Actor"/>
+	/// instance. A move is a single step.
+	/// </summary>
+    public class MoveCommand:CommandBase
+    {
+		Level level;
+
+		/// <summary>
+		/// Gets or sets the direction for the <see cref="Move"/>.
+		/// </summary>
+		/// <value>The direction that the move will take place.</value>
+		public Direction Direction
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MoveCommand"/> class.
+		/// </summary>
+		/// <param name="level">The level where the move will take place.</param>
+		/// <param name="direction">The direction of the move.</param>
+		public MoveCommand(Level level, Direction direction)
+		{
+			if (level == null)
+			{
+				throw new ArgumentNullException("level");
+			}
+			Direction = direction;
+			this.level = level;
+		}
+
+		/// <summary>
+		/// Executes this command.
+		/// Move the <see cref="Actor"/> one step
+		/// in the predefined <see cref="Direction"/>.
+		/// </summary>
+		public override void Execute()
+		{
+			Move move = new Move(Direction);
+			level.Actor.DoMove(move);
+		}
+
+		/// <summary>
+		/// Undoes that which was performed with <see cref="Execute"/>.
+		/// Steps back to the original location.
+		/// </summary>
+		public override void Undo()
+		{
+			level.Actor.UndoMove();
+		}
+
+		/// <summary>
+		/// Redoes this command after it has been undone. <see cref="Undo"/>.
+		/// </summary>
+		public override void Redo()
+		{
+			Execute();
+		}
+	}
+}
