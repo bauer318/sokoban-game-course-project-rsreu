ff5899fc6aeeb1b87c73ab10afc23f75f7f59b8f
Jacques kibamba <jacqueskibamba@gmail.com>
2022-12-23 20:30:41 +0300

    Base's new game model created

---
+++ Model/PlayGame/Commands/CommandManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.PlayGame.Commands
+{
+	/// <summary>
+	/// Provides for execution and undoing
+	/// of <see cref="CommandBase"/> instances.
+	/// </summary>
+	public class CommandManager
+	{
+		/// <summary>
+		/// Command's stack
+		/// </summary>
+		private Stack<CommandBase> _commandStack = new Stack<CommandBase>();
+		/// <summary>
+		/// Redo command's stack
+		/// </summary>
+		private Stack<CommandBase> _redoStack = new Stack<CommandBase>();
+		/// <summary>
+		/// indicates if can undo
+		/// </summary>
+		public static bool CanUndo;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CommandManager"/> class.
+		/// </summary>
+		public CommandManager()
+		{
+			CanUndo = true;
+		}
+		/// <summary>
+		/// Executes the specified command.
+		/// </summary>
+		/// <param name="parCommand">The command to execute.</param>
+		public void Execute(CommandBase parCommand)
+		{
+			_redoStack.Clear();
+			parCommand.Execute();
+			_commandStack.Push(parCommand);
+		}
+
+		/// <summary>
+		/// Undoes the execution of a previous <see cref="CommandBase"/>.
+		/// </summary>
+		public void Undo()
+		{
+			if (_commandStack.Count < 1 || !CanUndo)
+			{
+				return;
+			}
+			CanUndo = false;
+			CommandBase command = _commandStack.Pop();
+			command.Undo();
+			_redoStack.Push(command);
+		}
+		/// <summary>
+		/// Clears the command stacks.
+		/// </summary>
+		public void Clear()
+		{
+			_commandStack.Clear();
+			_redoStack.Clear();
+		}
+	}
+}
---
+++ ModelWPF/Game/Cells/CellBase.cs
@@ -0,0 +1,70 @@
+﻿using Model.PlayGame.Locations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.PlayGame.Cell
+{
+	/// <summary>
+	/// Base class for all cells in a Level.
+	/// </summary>
+	public abstract class CellBase
+	{
+		/// <summary>
+		/// The parName can be used to identify the type
+		/// of the cell without using GetType().
+		/// </summary>
+		private string _name;
+		/// <summary>
+		/// the location on the Level.
+		/// </summary>
+		private Location _location;
+		/// <summary>
+		/// Gets or sets the parName of this cell.
+		/// The parName can be used to identify the type
+		/// of the cell without using GetType().
+		/// </summary>
+		/// <value>The parName of the cell. 
+		/// The conceptual type parName of the cell, 
+		/// such as <em>Wall</em> or <em>Floor</em></value>
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				_name = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the location on the Level>.
+		/// </summary>
+		/// <value>The location of the cell on the Level</value>
+		public Location Location
+		{
+			get
+			{
+				return _location;
+			}
+			set
+			{
+				_location = value;
+			}
+		}
+		/// <summary>
+		/// CellBase's Constructor
+		/// </summary>
+		/// <param parName="name">Cell's Name</param>
+		/// <param parName="location">Cell's location</param>
+		public CellBase(string parName, Location parLocation)
+		{
+			_name = parName;
+			_location = parLocation;
+		}
+	}
+}
