c1abc7e0bc00533f16fb2f18c7397abc3617c272
Jacques kibamba <jacqueskibamba@gmail.com>
2022-12-24 16:34:24 +0300

    Added Game Model folder for Console

---
+++ ModelConsole/Game/NewGame/NewGameConsole.cs
@@ -0,0 +1,61 @@
+﻿using Model.PlayGame.Locations;
+using Model.PlayGame.NewGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelConsole.Game.NewGame
+{
+    public class NewGameConsole : NewGameBase
+    {
+        private GameState gameState;
+        /// <summary>
+        /// Gets the state of the game. That is, whether
+        /// it is running, loading etc.
+        /// <see cref="GameState"/>
+        /// </summary>
+        /// <value>The state of the game.</value>
+        public GameState GameState
+        {
+            get
+            {
+                return gameState;
+            }
+            private set
+            {
+                gameState = value;
+            }
+        }
+        public override void GotoNextLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool InBounds(Location parLocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Level_LevelCompleted(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LoadLevel(int parLevelNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RestartLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StartLevel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
