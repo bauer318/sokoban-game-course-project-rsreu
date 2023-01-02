26b9500a4a6e10fced6f3a0ff2289ae6fa6c5e2a
Jacques kibamba <jacqueskibamba@gmail.com>
2023-01-02 15:14:19 +0300

    initialized Record

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Model.GameRecord;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecordUtils u = new RecordUtils();
+            u.RecordManager = new RecordManager(new System.Collections.Generic.Dictionary<int, Record>());
+            u.UpdateRecord(0, 10000);
+        }
+    }
+}
