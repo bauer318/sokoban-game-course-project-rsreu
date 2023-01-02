a244547e3db263565ca30ddb24fe057dc1b04fa2
Jacques kibamba <jacqueskibamba@gmail.com>
2023-01-02 14:01:49 +0300

    moved view's logic from controller to view

--- Controller/PlayGame/ControllerPlayGame.cs
+++ Controller/PlayGame/ControllerPlayGame.cs
@@ -14,36 +14,20 @@ namespace Controller.PlayGame
     /// <summary>
     /// Base class for a New Game Controller
     /// </summary>
-    public abstract class ControllerPlayGame:ControllerNewGameRecordBase
+    public abstract class ControllerPlayGame : ControllerNewGameRecordBase
     {
         /// <summary>
         /// The base view for a new game
         /// </summary>
         public ViewNewGameBase ViewNewGameBase;
         /// <summary>
-        /// Provides for execution and undoing a command
-        /// </summary>
-        public readonly CommandManager CommandManager = new CommandManager();
-        /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="parViewNewGameBase">The base view for a new game</param>
-        public ControllerPlayGame(ViewNewGameBase parViewNewGameBase):base()
+        public ControllerPlayGame(ViewNewGameBase parViewNewGameBase) : base()
         {
             ViewNewGameBase = parViewNewGameBase;
         }
-        /// <summary>
-        /// Attempts to start the game's first level
-        /// </summary>
-        public abstract void TryToStartFirstLevel();
-        /// <summary>
-        ///Process drawing a game Level
-        /// </summary>
-        public abstract void ProcessDrawGameLevel();
-        /// <summary>
-        /// Set the level's cell style
-        /// </summary>
-        public abstract void SetCellButtonStyle();
         /// <summary>
         /// Update a record after that a level has been completed succefuly
         /// </summary>
