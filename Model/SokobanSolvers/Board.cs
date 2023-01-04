0edc704a6b57d9bc4a86efde4d4f0d5d40477352
Jacques kibamba <jacqueskibamba@gmail.com>
2023-01-04 15:45:17 +0300

    Solver long work

---
+++ Model/SokobanSolvers/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.SokobanSolvers
+{
+    public class Board
+    {
+        public string Cur { get;  set; }
+        public string Sol { get;  set; }
+        public int X { get;  set; }
+        public int Y { get;  set; }
+
+        public Board(string cur, string sol, int x, int y)
+        {
+            Cur = cur;
+            Sol = sol;
+            X = x;
+            Y = y;
+        }
+    }
+}
