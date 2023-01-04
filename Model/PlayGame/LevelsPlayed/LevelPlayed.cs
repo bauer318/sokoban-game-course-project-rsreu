c4096232b0c4bf017c4538c26ba14b83e8b2a537
Jacques kibamba <jacqueskibamba@gmail.com>
2023-01-04 18:02:33 +0300

    Added the played levels

---
+++ Model/PlayGame/LevelsPlayed/LevelPlayed.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.PlayGame.LevelsPlayed
+{
+    [Serializable]
+    public class LevelPlayed
+    {
+        public List<int> LevelsPlayed { get; set; }
+
+        public LevelPlayed(List<int> parLevelsPlayed)
+        {
+            LevelsPlayed = parLevelsPlayed;
+        }
+        
+
+        public void AddLevelPlayed(int parLevelNumber)
+        {
+            if (!IsLevelPlayed(parLevelNumber))
+            {
+                LevelsPlayed.Add(parLevelNumber);
+            }
+        }
+
+        public bool IsLevelPlayed(int parLevelNumber)
+        {
+            return LevelsPlayed.Contains(parLevelNumber);
+        }
+
+        public int GetLastLevelPlayed()
+        {
+            if (LevelsPlayed.Count > 0)
+            {
+                return LevelsPlayed.Last();
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+
+        public void ReInitializeLevelsPlayed()
+        {
+            LevelsPlayed = new List<int>();
+        }
+
+
+    }
+}
